2f4ef8a1b10e8ef592a8192f968f060cc78e0a55
Mahmudul Hoque <m.hoque@gmail.com>
2020-12-21 22:36:15 +0000

    fetch members from server

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,12 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-//import axios from 'axios'
 
 import Navigation from './components/navigation';
 import Members from './components/members';
 //import Dashboard from './components/dashboard';
 
 class App extends Component {
-
-    componentDidMount() {
-      //axios.get('/api/v1/say-something').then((res) => {
-      //  const response = res.data;
-      //  this.setState({response});
-      //});
-    }
 
   render() {
     return (
