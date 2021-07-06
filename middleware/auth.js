8a33e082796e30b333e3d20b7532439950dd0fbf
Mahmudul Hoque <mahmud@Mahmuduls-Mac-mini.local>
2021-07-06 18:17:34 +0100

    logging

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,11 +4,13 @@ module.exports = {
             console.log(req.session.user);
             return next();
         } else {
+            console.log("Unauthorized User");
             res.status(401).send("Unauthorized");
         }
     },
     ensureGuest: function (req, res, next) {
         if(req.session.user) {
+            console.log(req.session.user);
             res.status(403).send("Forbidden");
         } else {
             return next();
